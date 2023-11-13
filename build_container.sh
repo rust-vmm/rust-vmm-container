6edae3a7065e22c81b71751b55aef040c07880da
Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
2023-11-13 20:31:20 +0200

    build_container.sh: use eatmydata for apt operations
    eatmydata disables fsync and similar operations to speed up data
    operations for ephemeral tasks (like building a container image).
    
    See: https://packages.debian.org/bookworm/eatmydata
    
    Signed-off-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>

--- build_container.sh
+++ build_container.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-apt-get update
-
 # DEBIAN_FRONTEND is set for tzdata.
-DEBIAN_FRONTEND="noninteractive" apt-get install --no-install-recommends -y \
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update && \
+  apt-get install -y eatmydata
+
+
+eatmydata apt-get install --no-install-recommends -y \
     curl gcc git python3 python3-pip shellcheck \
     libssl-dev tzdata cmake g++ pkg-config jq libcurl4-openssl-dev libelf-dev \
     libdw-dev binutils-dev libiberty-dev make \
@@ -15,9 +19,9 @@ DEBIAN_FRONTEND="noninteractive" apt-get install --no-install-recommends -y \
     debhelper-compat libdbus-1-dev libglib2.0-dev meson ninja-build dbus
 
 # cleanup
-apt-get clean && rm -rf /var/lib/apt/lists/*
+eatmydata apt-get clean && rm -rf /var/lib/apt/lists/*
 
-pip3 install --no-cache-dir pytest pexpect boto3 pytest-timeout && apt purge -y python3-pip
+pip3 install --no-cache-dir pytest pexpect boto3 pytest-timeout && eatmydata apt purge -y python3-pip
 
 # Install rustup and a fixed version of Rust.
 curl https://sh.rustup.rs -sSf | sh -s -- \
