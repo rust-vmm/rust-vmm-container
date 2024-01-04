cd03e0ca697feb861f925865ba61ae37924e64b3
Egor Lazarchuk <yegorlz@amazon.co.uk>
2024-01-04 17:01:09 +0000

    fix: replaced ARCH with `uname -m`
    In the previous commits the ARCH
    env variable was introduced to the Dockerfile
    and build_container.sh scripts. It was meant to
    help with configuration of linux headers for
    musl-tools, but did not work for some reason.
    Considering the build_container.sh was already
    using `uname -m` calls to get the current arch
    this commit also uses it, so there is no need
    for ARCH anymore.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- build_container.sh
+++ build_container.sh
@@ -18,8 +18,8 @@ DEBIAN_FRONTEND="noninteractive" apt-get install --no-install-recommends -y \
 apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # help musl-gcc find linux headers
-cd /usr/include/$ARCH-linux-musl \
-    && ln -s ../$ARCH-linux-gnu/asm asm \
+cd /usr/include/$(uname -m)-linux-musl \
+    && ln -s ../$(uname -m)-linux-gnu/asm asm \
     && ln -s ../linux linux \
     && ln -s ../asm-generic asm-generic
 
