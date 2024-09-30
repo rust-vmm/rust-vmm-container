610e0e9c2765c78741fe29fa25e4e6124bde982a
Ruoqing He <heruoqing@iscas.ac.cn>
2024-09-30 10:39:53 +0000

    riscv64: Mount `tmpfs` to support unit-tests expecting `/tmp`
    As `9p` rootfs documented [1], mount a `tmpfs` onto `/tmp` could avoid
    `E: Unable to determine file size for fd 7 - fstat (2: No such file or
    directory)`.
    
    And various unittests in `linux-loader`, `vm-memory` and etc. explicitly
    expect `/tmp` to exist, and were failing on riscv because no `tmpfs` was
    mounted.
    
    Modify `/etc/fstab` to apply the mount operation during boot.
    
    [1] https://wiki.qemu.org/Documentation/9p_root_fs#Let's_start_the_Installation
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- riscv64/build_finalize.sh
+++ riscv64/build_finalize.sh
@@ -7,6 +7,10 @@ DEBIAN_FRONTEND="noninteractive" apt-get install --no-install-recommends -y \
     openssh-client libslirp-dev libfdt-dev libglib2.0-dev libssl-dev \
     libpixman-1-dev netcat
 
+# Modify fstab to mount `tmpfs` during boot
+# See: https://wiki.qemu.org/Documentation/9p_root_fs#Let's_start_the_Installation
+echo 'tmpfs /tmp tmpfs rw,nosuid,nodev,size=524288k,nr_inodes=204800 0 0' >> $ROOTFS_DIR/etc/fstab
+
 # Setup container ssh config
 yes "" | ssh-keygen -P ""
 cat /root/.ssh/id_rsa.pub > $ROOTFS_DIR/root/.ssh/authorized_keys
